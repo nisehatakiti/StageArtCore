f664da489498cf3a5e0028e2f711e1d694c3a6e2
nisehatakiti <nisehatakiti@gmail.com>
2026-09-17 13:30:36 +0900

    Fix plugin author URL

--- stageart-core.php
+++ stageart-core.php
@@ -6,7 +6,7 @@
  * Requires at least: 6.0
  * Requires PHP: 8.0
  * Author: nisehatakiti
- * Author URI: https://nisehatakiti.online.html
+ * Author URI: https://nisehatakiti.online/
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: stageart-core
