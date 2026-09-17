c6dc99cc0fec632d8527594b3db6699f42b6186d
nisehatakiti <nisehatakiti@gmail.com>
2026-09-17 09:14:32 +0900

    stageart: add theme CSS bridge

---
+++ theme/stageart/theme.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+if(!defined('ABSPATH'))exit;
+
+use StageArtCore\Presentation\PublicSite\ThemeRenderer;
+
+function stageart_theme_print_theme_css():void{
+    if(is_admin()||!class_exists(ThemeRenderer::class))return;
+    $productionId=0;
+    $slug=get_query_var('stageart_production_slug');
+    if(is_string($slug)&&$slug!==''){
+        $q=new WP_Query(['post_type'=>'stageart_production','name'=>$slug,'post_status'=>'publish','posts_per_page'=>1,'fields'=>'ids']);
+        if($q->posts)$productionId=(int)$q->posts[0];
+    }
+    $css=$productionId?ThemeRenderer::productionCss($productionId):ThemeRenderer::organizationCss();
+    if($css)echo'<style id="stageart-theme-vars">'.$css.'</style>';
+}
+add_action('wp_head','stageart_theme_print_theme_css',20);
