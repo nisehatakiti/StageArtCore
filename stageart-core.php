f0a5541264b94964265ffb0274b0a07afe3e28f1
nisehatakiti <nisehatakiti@gmail.com>
2026-09-18 10:12:52 +0900

    Bump StageArtCore to 0.7.0

--- stageart-core.php
+++ stageart-core.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: StageArtCore
  * Description: 舞台芸術団体向けの公演・メンバー・アンケート管理基盤。
- * Version: 0.6.9
+ * Version: 0.7.0
  * Requires at least: 6.0
  * Requires PHP: 8.0
  * Author: nisehatakiti
